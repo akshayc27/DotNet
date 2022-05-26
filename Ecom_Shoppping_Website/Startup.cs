050aa089a61e18b4ab5b0b1042fd4661249e50b1
akshayc27 <akshaychacharkar27@gmail.com>
2022-05-26 11:01:57 +0530

    Code Changes.

--- Ecom_Shoppping_Website/Startup.cs
+++ Ecom_Shoppping_Website/Startup.cs
@@ -30,7 +30,7 @@ namespace Ecom_Shoppping_Website
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
