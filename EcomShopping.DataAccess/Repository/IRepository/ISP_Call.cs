389032bf387d1234f6ef6aa8d40fc74f19011fd2
akshayc27 <akshaychacharkar27@gmail.com>
2022-05-27 18:43:32 +0530

    Added Category Controller

--- EcomShopping.DataAccess/Repository/IRepository/ISP_Call.cs
+++ EcomShopping.DataAccess/Repository/IRepository/ISP_Call.cs
@@ -15,7 +15,7 @@ namespace EcomShopping.DataAccess.Repository.IRepository
 
         IEnumerable<T> List<T>(string procedureName, DynamicParameters param = null);
 
-        Tuple<IEnumerable<T1>,IEnumerable<T2>> List<T1, T2>(string procedureName, DynamicParameters param = null);)
+        Tuple<IEnumerable<T1>,IEnumerable<T2>> List<T1, T2>(string procedureName, DynamicParameters param = null);
 
     }
 }
---
+++ Ecom_Shoppping_Website/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,43 @@
+﻿using EcomShopping.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcomShopping.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+
+            var allObj = _unitOfWork.Category.GetAll();
+
+            return Json(new { data = allObj });
+        }
+
+        #endregion
+
+
+
+    }
+}
