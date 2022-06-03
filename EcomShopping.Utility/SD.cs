f1bc2ff7f5fe4ad3d4d25e19aa60f4fee6338615
akshayc27 <akshaychacharkar27@gmail.com>
2022-06-03 12:17:53 +0530

    Scanfoldded Identity pages.

--- EcomShopping.Utility/SD.cs
+++ EcomShopping.Utility/SD.cs
@@ -6,5 +6,9 @@ namespace EcomShopping.Utility
 {
     public static class SD
     {
+        public const string Role_User_indi = "Individual Customer";
+        public const string Role_User_Comp = "Company Customer";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
     }
 }
